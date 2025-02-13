abb3a970abcc07cb265380497211cef517880d73
KZielinskii <58587948+KZielinskii@users.noreply.github.com>
2025-02-13 05:48:14 +0100

    Update

--- Kompilator/Program.asm
+++ Kompilator/Program.asm
@@ -1,33 +1,29 @@
 	jump.i #lab0	 ;jump.i  lab0
-gcd:
-	enter.i	#12	 ; enter.i 12
-	je.i	*BP+12,#0,#lab1	 ; je.i b,0,lab1
-	mov.i	#0,BP-4	 ; mov.i 0,t0
+lab0:
+	realtoint.r	#12.12,40	 ; realtoint.r 12.12,t0
+	mov.i	40,32	 ; mov.i t0,a
+	realtoint.r	#31.12,44	 ; realtoint.r 31.12,t1
+	mov.i	44,36	 ; mov.i t1,b
+	mov.r	#12.21,16	 ; mov.r 12.21,x
+	mov.r	#42.123,24	 ; mov.r 42.123,y
+	write.i	32	 ; write.i a
+	write.i	36	 ; write.i b
+	write.r	16	 ; write.r x
+	write.r	24	 ; write.r y
+	je.i	#0,#0,#lab1	 ; je.i 0,0,lab1
+	mov.i	#0,48	 ; mov.i 0,t2
 	jump.i	#lab2	 ; jump.i lab2
 lab1:
-	mov.i	#1,BP-4	 ; mov.i 1,t0
+	mov.i	#1,48	 ; mov.i 1,t2
 lab2:
-	je.i	BP-4,#0,#lab3	 ; je.i t0,0,lab3
-	mov.i	*BP+16,*BP+8	 ; mov.i a,gcd
+	mov.i	48,32	 ; mov.i t2,a
+	write.i	32	 ; write.i a
+	je.i	#112,#0,#lab3	 ; je.i 112,0,lab3
+	mov.i	#0,52	 ; mov.i 0,t3
 	jump.i	#lab4	 ; jump.i lab4
 lab3:
-	mod.i	*BP+16,*BP+12,BP-8	 ; mod.i a,b,t1
-	push.i	 BP+12	 ; push.i &b
-	push.i	#BP-8	 ; push.i &t1
-	push.i	#BP-12	 ; push.i &t2
-	call.i	#gcd	; call.i &gcd
-	incsp.i	#12	 ; incsp.i 12
-	mov.i	BP-12,*BP+8	 ; mov.i t2,gcd
+	mov.i	#1,52	 ; mov.i 1,t3
 lab4:
-	leave		 ; leave 
-	return		 ; return 
-lab0:
-	read.i	8	 ; read.i x
-	read.i	12	 ; read.i y
-	push.i	#8	 ; push.i &x
-	push.i	#12	 ; push.i &y
-	push.i	#16	 ; push.i &t3
-	call.i	#gcd	; call.i &gcd
-	incsp.i	#12	 ; incsp.i 12
-	write.i	16	 ; write.i t3
+	mov.i	52,32	 ; mov.i t3,a
+	write.i	32	 ; write.i a
 	exit	;exit
--- Kompilator/tests/test.pas
+++ Kompilator/tests/test.pas
@@ -5,8 +5,8 @@ function funname(a, b: integer): integer;
 var lo, la: integer;
 
 begin
-la := a+1;
-if a > 5 then
+la := a + 1 * 2 / (12 - 9) mod 4 div 2;
+if  a > 5  then
     b := 2
 else
     la := funname(a+2, b+2);
@@ -19,6 +19,8 @@ read(x, y);
 y := funname(x, 12);
 write(y) 
 end.
+
+
 
 ==============================================
 
@@ -40,4 +42,31 @@ read(x, y);
 
 write(gcd(x, y)) end.
 
+
+
 =============================================
+
+program example(input, output);
+
+var x, y: real;
+var a, b: integer;
+
+begin
+
+a:=12.12;
+b:=31.12;
+x:=12.21;
+y:=42.123;
+write(a,b,x,y);
+
+a:= not 0;
+write(a);
+a:= not 112;
+write(a)
+
+end.
+
+
+
+
+
