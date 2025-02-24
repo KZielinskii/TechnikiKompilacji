2084098c520420a7833139286ca694ce3d21a86e
KZielinskii <58587948+KZielinskii@users.noreply.github.com>
2025-02-25 00:42:34 +0100

    Update Program.asm

--- Kompilator/Program.asm
+++ Kompilator/Program.asm
@@ -1,11 +1,33 @@
 	jump.i #lab0	 ;jump.i  lab0
+gcd:
+	enter.i	#12	 ; enter.i 12
+	je.i	*BP+12,#0,#lab1	 ; je.i b,0,lab1
+	mov.i	#0,BP-4	 ; mov.i 0,t0
+	jump.i	#lab2	 ; jump.i lab2
+lab1:
+	mov.i	#1,BP-4	 ; mov.i 1,t0
+lab2:
+	je.i	BP-4,#0,#lab3	 ; je.i t0,0,lab3
+	mov.i	*BP+16,*BP+8	 ; mov.i a,gcd
+	jump.i	#lab4	 ; jump.i lab4
+lab3:
+	mod.i	*BP+16,*BP+12,BP-8	 ; mod.i a,b,t1
+	push.i	 BP+12	 ; push.i &b
+	push.i	#BP-8	 ; push.i &t1
+	push.i	#BP-12	 ; push.i &t2
+	call.i	#gcd	; call.i &gcd
+	incsp.i	#12	 ; incsp.i 12
+	mov.i	BP-12,*BP+8	 ; mov.i t2,gcd
+lab4:
+	leave		 ; leave 
+	return		 ; return 
 lab0:
-	mov.i	#1,8	 ; mov.i 1,a
-	mov.i	#0,12	 ; mov.i 0,b
-	and.i	8,12,24	 ; and.i a,b,t0
-	mov.i	24,16	 ; mov.i t0,c
-	or.i	8,12,28	 ; or.i a,b,t1
-	mov.i	28,20	 ; mov.i t1,d
-	write.i	16	 ; write.i c
-	write.i	20	 ; write.i d
+	read.i	8	 ; read.i x
+	read.i	12	 ; read.i y
+	push.i	#8	 ; push.i &x
+	push.i	#12	 ; push.i &y
+	push.i	#16	 ; push.i &t3
+	call.i	#gcd	; call.i &gcd
+	incsp.i	#12	 ; incsp.i 12
+	write.i	16	 ; write.i t3
 	exit	;exit
